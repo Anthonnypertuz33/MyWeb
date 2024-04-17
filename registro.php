01a59f6f1932895391b33ffc58f0785d44406941
Anthonnypertuz33 <69432774+Anthonnypertuz33@users.noreply.github.com>
2024-04-17 11:08:32 -0500

    .

---
+++ registro.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de Correo Electrónico</title>
+</head>
+<body>
+    <h1>Registro de Correo Electrónico</h1>
+    <form action="guardar.php" method="post">
+        <label for="email">Correo Electrónico:</label><br>
+        <input type="email" id="email" name="email" required><br><br>
+        <input type="submit" value="Registrar" name="registrar">
+    </form>
+</body>
+</html>
